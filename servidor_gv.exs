dba2a5b2cdf673f017c38027213bca1098813413
Haz <jsalamerosanz@gmail.com>
2019-12-18 16:50:12 +0100

    A VER SI HACES ALGO CRACK

--- servidor_gv.exs
+++ servidor_gv.exs
@@ -15,7 +15,7 @@ defmodule ServidorGV do
     # COMPLETAR  con lo campos necesarios para gestionar
     # el estado del gestor de vistas
     defstruct vista: %{num_vista: 0, primario: :undefined, copia: :undefined},
-            tentativa: %{num_vista:0, primario: :undefined, copia: :undefined},
+            tentativa: %{num_vista: 0, primario: :undefined, copia: :undefined},
             resto_nodos: [], #lista que contien tuplas {nodo, numLatidos}
             primario_latido: @latidos_fallidos+1,
             copia_latido: @latidos_fallidos+1
@@ -45,7 +45,7 @@ defmodule ServidorGV do
         Generar un estructura de datos vista inicial
     """
     def vista_inicial() do
-        %{ServidorGv}
+        %ServidorGv{}
     end
 
     @doc """
@@ -64,7 +64,7 @@ defmodule ServidorGV do
     @spec startService(node) :: boolean
     def startService(nodoElixir) do
         NodoRemoto.esperaNodoOperativo(nodoElixir, __MODULE__)
-        
+
         # Poner en marcha el código del gestor de vistas
         Node.spawn(nodoElixir, __MODULE__, :init_sv, [])
    end
@@ -97,52 +97,50 @@ defmodule ServidorGV do
                             0 ->IO.ANSI.blue <>"No hay primario, inicializo" |> IO.puts
                                 estado = %{estado_sistema | tentativa: %{estado_sistema.tentativa |
                                                             num_vista: 1,
-                                                            primario: nodo_emisor,  
+                                                            primario: nodo_emisor,
                                                             copia: :undefined},
-                                            primario_copia = @latidos_fallidos+1
+                                                            primario_latido: @latidos_fallidos+1
                                             }
                                 #mando la vista pero no es valida por ahora
                                 send({:cliente_gv, nodo_emisor}, {:vista_tentativa , estado.tentativa, false})
                                 estado
                             1 ->  IO.ANSI.blue <> "Hay primario, no hay copia" |> IO.puts
                                 cond do
-                                    nodo_emisor == estado_sistema.tentativa.primario -> 
+                                    nodo_emisor == estado_sistema.tentativa.primario ->
                                         #el que ha hecho latido es el primario
                                         IO.ANSI.green <>  "Tengo primario, y primario hace el latido de nuevo" |> IO.puts
                                         #envio
                                         send({:cliente_gv, nodo_emisor}, {:vista_tentativa , estado_sistema.tentativa, false})
                                         #reinicio latidos ya qu sigue vivo
-                                        %{estado_sistema | primario_latido = @latidos_fallidos+1}
-
-                                    n_vista_latido == 0 -> 
-                                         IO.ANSI.green <> "El nodo que hace latido es nuevo" |> IO.puts
+                                        %{estado_sistema | primario_latido: @latidos_fallidos+1}
+                                    n_vista_latido == 0 ->
+                                        IO.ANSI.green <> "El nodo que hace latido es nuevo" |> IO.puts
                                         estado = %{estado_sistema | tentativa: %{estado_sistema.tentativa | num_vista: (estado_sistema.tentativa.num_vista+1), copia: nodo_emisor},
-                                        copia_latido = @latidos_fallidos+1
-                                        }
+                                                                    copia_latido:  @latidos_fallidos+1
+                                                                }
                                         send({:cliente_gv, nodo_emisor}, {:vista_tentativa , estado.tentativa, true})
                                         ##################################
                                         # valido???
-                                        %(estado | vista: estado.tentativa)
-                                        ################################## 
+                                        %{estado | vista: estado.tentativa}
+                                        ##################################
                                     true ->
                                         IO.ANSI.red <> "No deberia sacar este caso. \n Vista = 1, nodo: late con latido != 0 y no existe aun la copia" <> IO.ANSI.reset |> IO.puts
                                 end
                             _ -> IO.ANSI.blue <> "Vista != 1 y 0" <> IO.ANSI.reset |> IO.puts
                                 cond do
-                                    nodo_emisor == estado_sistema.tentativa.primario -> 
+                                    nodo_emisor == estado_sistema.tentativa.primario ->
                                         IO.ANSI.pink <> "PRIMARIO" <> IO.ANSI.reset |> IO.puts
                                         estadoNuevo = cond do
                                             n_vista_latido == estado_sistema.tentativa.num_vista ->
                                                 IO.ANSI.green <> "Primario confirma la vista" |> IO.puts
                                                 #confirmo vista y reinicio los latidos
-                                                %{estado_sistema | vista: estado_sistema.tentativa, primario: @latidos_fallidos+1}
+                                                %{estado_sistema | vista: estado_sistema.tentativa, primario_latido: @latidos_fallidos+1}
                                             n_vista_latido == 0 ->
                                                 IO.ANSI.red <> "*********PRIMARIO CAIDO***********" |> IO.puts
-                                                estado_promocionar = estado_sistema
-                                                promocion(estado_promocionar)
-                                            true -> 
+                                                promocion(estado_sistema)
+                                            true ->
                                                 IO.ANSI.green <> "Primario vive" |> IO.puts
-                                                %{estado_sistema | primario: @latidos_fallidos+1}
+                                                %{estado_sistema | primario_latido: @latidos_fallidos+1}
                                         end
                                         consistencia = (estadoNuevo.tentativa == estadoNuevo.vista)
                                         send({:cliente_gv, nodo_emisor}, {:vista_tentativa , estadoNuevo.tentativa, consistencia})
@@ -152,10 +150,9 @@ defmodule ServidorGV do
                                         estadoNuevo = condo do
                                             n_vista_latido == 0 ->
                                                 IO.ANSI.red <> "*********COPIA CAIDO***********" |> IO.puts
-                                                estado_promocionar = estado_sistema
-                                                promocionCopia(estado_promocionar)
+                                                promocionCopia(estado_sistema)
                                             true ->
-                                                %{estado_sistema | primario: @latidos_fallidos+1}
+                                                %{estado_sistema | copia_latido: @latidos_fallidos+1}
                                         end
                                         consistencia = (estadoNuevo.tentativa == estadoNuevo.vista)
                                         send({:cliente_gv, nodo_emisor}, {:vista_tentativa , estadoNuevo.tentativa, consistencia})
@@ -165,7 +162,7 @@ defmodule ServidorGV do
                                         IO.ANSI.pink <> "NO PRIMARIO NO COPIA" <> IO.ANSI.reset |> IO.puts
                                         estadoNuevo = if (n_vista_latido==0) do
                                                         IO.ANSI.green <> "Registro nodo como espera" |> IO.puts
-                                                        %{estado_sistema | resto_nodos: resto_nodos ++ [{nodo_emisor, @latidos_fallidos+1}]
+                                                        %{estado_sistema | resto_nodos: resto_nodos ++ [{nodo_emisor, @latidos_fallidos+1}]}
                                         else
                                             IO.ANSI.green <> "Actualizo nodo en espera pq sigue vivo" |> IO.puts
                                             nuevaListaNodos = List.keyreplace?(estado_sistema.resto_nodos, nodo_emisor, 0, {nodo_emisor,@latidos_fallidos+1})
@@ -180,12 +177,11 @@ defmodule ServidorGV do
                             consistencia = (estadoNuevo.tentativa == estadoNuevo.vista)
                             send({:cliente_gv, nodo_emisor}, {:vista_tentativa , estado_sistema.tentativa, consistencia})
                             estado_sistema
-                        
+
                     :procesa_situacion_servidores ->
-                        estado=if (length(estado_sistema.resto_nodos) > 0) do
-                            resto_nodos = for i <- resto_nodos, do
-                                {elem(i,0), elem(i,1)-1}
-                            end
+                        estado= if (length(estado_sistema.resto_nodos) > 0) do
+                                resto_nodos =   for i <- resto_nodos, do: {elem(i,0), elem(i,1)-1}
+                                end
 
                             resto_nodos = borrarInactivos(resto_nodos)
 
@@ -193,15 +189,13 @@ defmodule ServidorGV do
                             copiaInactivo = (estado_sistema.copia_latido == 0)
                             estadoNuevo = cond do
                                 primarioInactivo && copiaInactivo ->
-                                    IO.ANSI.red <> "##############PRIMARIO CAIDO################" |> IO.puts
+                                    IO.ANSI.red <> "##############PRIMARIO y COPIA CAIDO################" |> IO.puts
                                     vista_inicial()
                                 primarioInactivo ->
                                     IO.ANSI.pink <> "### PRIMARIO CAIDO - COPIA VIVE ###" |> IO.puts
-                                    estado_promocionar = estado_sistema
                                     promocion(estado_sistema)
                                 copiaInactivo ->
                                     IO.ANSI.pink <> "### COPIA CAIDO - PRIMARIO VIVE ###" |> IO.puts
-                                    estado_promocionar = estado_sistema
                                     promocioncopia(estado_sistema)
                             end
                             estadoNuevo
@@ -211,20 +205,24 @@ defmodule ServidorGV do
                         end
                         estado
         end
-
         bucle_recepcion(estado)
     end
-    
+
     defp promocion(estado_sist) do
         IO.ANSI.blue <> "CREANDO NUEVO PRIMARIO" |> IO.puts
-        %{estado_sist | tentativa: %{estado_sist.tentativa | primario: estado_sist.tentativa.copia, copia: :undefined, primario_latido: estado_sist.tentativa.copia_latido}}
+        if (estado_sist.tentativa == estado_sist.vista) do
+            %{estado_sist | tentativa: %{estado_sist.tentativa | primario: estado_sist.tentativa.copia, copia: :undefined, primario_latido: estado_sist.tentativa.copia_latido}}
+            promocionCopia(estado_sist)
+        else
+            #CRASH
+            IO.ANSI.red <> "##############PRIMARIO y COPIA CAIDO################" |> IO.puts
+            vista_inicial()
+        end
     end
 
     defp promocionCopia(estado_sist) do
         IO.ANSI.blue <> "CREANDO NUEVA COPIA" |> IO.puts
-        estado = if estado.tentativa.primario == :undefined do
-                %{estado_sist | tentativa: %{estado_sist.tentativa | num_vista: (estado_sist.tentativa.num_vista + 1), copia: :undefined}}
-            else
+        estado = if estado.tentativa.primario != :undefined do
                 len = length(resto_nodos)
                 estadoNuevo = cond do
                     len == 0 ->
@@ -232,20 +230,19 @@ defmodule ServidorGV do
                         IO.ANSI.green <> "NO HAY NODOS PARA SUSTITUIR A COPIA" |> IO.puts
                         %{estado_sist | tentativa: %{estado_sist.tentativa | num_vista: (estado_sist.tentativa.num_vista + 1), copia: :undefined}}
                     true ->
-                        #hay nodos para sustituir a copia
+                        #hay nodos para sustituir a copiao
                         IO.ANSI.blue <> "SUSTITUYO A COPIA" |> IO.puts
                         #cojo la tupla
                         {nodoNuevo, latidosNuevo} = List.first(resto_nodos)
                         #quito primer elemento de la lista porque será la copia
                         nuevaLista = List.delete_at(resto_nodos,0)
-                        %{estado_sist | tentativa: %{estado_sist.tentativa | num_vista: (estado_sist.tentativa.num_vista + 1), primario: estado_sist.tentativa.copia, copia: nodoNuevo},
-                                     primario_latido: estado_sistema.tentativa.copia_latido, 
+                        %{estado_sist | tentativa: %{estado_sist.tentativa | num_vista: (estado_sist.tentativa.num_vista + 1), copia: nodoNuevo},
                                      copia_latido: @latidos_fallidos+1,
                                     resto_nodos: nuevaLista}
                 end
                 estadoNuevo
             end
-            
+
     end
 
 end
